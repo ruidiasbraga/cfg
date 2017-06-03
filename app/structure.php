2a1319e17ca44bc3596fa3c67aef19ae3d529758
ruidiasbraga <ruidiasbraga@gmail.com>
2017-06-03 13:51:11 +0100

    minor changes

--- app/structure.php
+++ app/structure.php
@@ -7,70 +7,17 @@
  
  /* menu structure for your files */
 $structure_main = '[
-                        {"fileName":"File_1"},
-                        {"fileName":"File_2"},
-                        {"fileName":"File_3"},
-                        {"fileName":"File_4"}
+                        {"fileName":"File_1"}
                     ]';
 
 /* file mandatory fields */
 $structure_1 = '[{
-                    "fieldName":"Field_1",
-                    "validationType":"number"
-                }]';
-
-
-$structure_2 = '[{
                         "fieldName": "Field_1",
                         "validationType": "text"
                     }, {
                         "fieldName": "Field_2",
                         "validationType": "text"
                 }]';
-
-$structure_3 = '[{
-                        "fieldName": "Field_1",
-                        "validationType": "number"
-                    }, {
-                        "fieldName": "Field_2",
-                        "validationType": "number"
-                }, {
-                        "fieldName": "Field_3",
-                        "validationType": "text"
-                }]';
-
-$structure_4 = '[{
-                        "fieldName": "Field_1",
-                        "validationType": "number"
-                    }, {
-                        "fieldName": "Field_2",
-                        "validationType": "number"
-                }, {
-                        "fieldName": "Field_3",
-                        "validationType": "text"
-                }, {
-                        "fieldName": "Field_4",
-                        "validationType": "text"
-                }, {
-                        "fieldName": "Field_5",
-                        "validationType": "text"
-                }, {
-                        "fieldName": "Field_6",
-                        "validationType": "text"
-                }, {
-                        "fieldName": "Field_7",
-                        "validationType": "text"
-                }, {
-                        "fieldName": "Field_8",
-                        "validationType": "text"
-                }, {
-                        "fieldName": "Field_9",
-                        "validationType": "text"
-                }, {
-                        "fieldName": "Field_10",
-                        "validationType": "number"
-                }]';
-
 
 	if (isset($_GET['struct_name']) && $_GET['struct_name'] != "") {
 		$struct_name = $_GET['struct_name'];
@@ -95,15 +42,6 @@ $structure_4 = '[{
 			break;
 		case 'File_1':
 			$struct_format = $structure_1;
-			break;
-		case 'File_2':
-			$struct_format = $structure_2;
-			break;
-		case 'File_3':
-			$struct_format = $structure_3;
-			break;
-		case 'File_4':
-			$struct_format = $structure_4;
 			break;
 		default:
 			$struct_format = "";
